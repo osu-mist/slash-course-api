bc2f9f944005a61f43051ff5ed237172dccdd0b8
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2016-04-20 08:39:41 -0700

    Add core classes

---
+++ src/main/groovy/edu/oregonstate/mist/slashcourses/core/ErrorPOJO.groovy
@@ -0,0 +1,15 @@
+package edu.oregonstate.mist.slashcourses.core
+
+/**
+ * Error object
+ */
+class ErrorPOJO {
+
+    Integer errorCode
+    String errorMessage
+
+    public ErrorPOJO(Integer errorCode, String errorMessage){
+        this.errorCode    = errorCode
+        this.errorMessage = errorMessage
+    }
+}
---
+++ src/main/groovy/edu/oregonstate/mist/slashcourses/core/SlashCourse.groovy
@@ -0,0 +1,17 @@
+package edu.oregonstate.mist.slashcourses.core
+
+/**
+ * Slash Course Class
+ */
+class SlashCourse {
+    Integer crn
+    String course_num
+    String course_name
+    Integer slash
+    String term
+    String instructor
+    String day
+    String time
+    String location
+    String type
+}
