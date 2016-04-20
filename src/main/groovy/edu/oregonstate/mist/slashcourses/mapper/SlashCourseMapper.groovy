4d1b623a690e32996cc68cc4424e656523da1969
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2016-04-20 08:47:32 -0700

    Add SlashCourseMapper

---
+++ src/main/groovy/edu/oregonstate/mist/slashcourses/mapper/SlashCourseMapper.groovy
@@ -0,0 +1,28 @@
+package edu.oregonstate.mist.slashcourses.mapper
+
+import edu.oregonstate.mist.slashcourses.core.SlashCourse
+import org.skife.jdbi.v2.StatementContext
+import org.skife.jdbi.v2.tweak.ResultSetMapper
+import java.sql.ResultSet
+import java.sql.SQLException
+
+/**
+ * Slash Course Mapper
+ */
+public class SlashCourseMapper implements ResultSetMapper<SlashCourse>{
+    @Override
+    SlashCourse map(int index, ResultSet r, StatementContext ctx) throws SQLException {
+        new SlashCourse(
+                crn:         r.getInt('CRN'),
+                course_num:  r.getString('COURSE_NUM'),
+                course_name: r.getString('COURSE_NAME'),
+                slash:       r.getInt('SLASH'),
+                term:        r.getString('TERM'),
+                instructor:  r.getString('INSTRUCTOR'),
+                day:         r.getString('DAY'),
+                time:        r.getString('TIME'),
+                location:    r.getString('LOCATION'),
+                type:        r.getString('TYPE')
+        )
+    }
+}
