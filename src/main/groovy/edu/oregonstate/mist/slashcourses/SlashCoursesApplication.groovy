baa615a49813c8e42a1e9424cc757bd4e6eb02ec
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2016-04-28 14:29:34 -0700

    Loaded properties, read error msg correctly

--- src/main/groovy/edu/oregonstate/mist/slashcourses/SlashCoursesApplication.groovy
+++ src/main/groovy/edu/oregonstate/mist/slashcourses/SlashCoursesApplication.groovy
@@ -2,8 +2,9 @@ package edu.oregonstate.mist.slashcourses
 
 import edu.oregonstate.mist.slashcourses.db.InstructorDAO
 import edu.oregonstate.mist.slashcourses.db.SlashCourseDAO
-
+import edu.oregonstate.mist.api.Resource
 import edu.oregonstate.mist.slashcourses.resources.SlashCourseResource
+
 import io.dropwizard.Application
 import io.dropwizard.jdbi.DBIFactory
 import io.dropwizard.setup.Bootstrap
@@ -31,6 +32,7 @@ class SlashCoursesApplication extends Application<SlashCoursesApplicationConfigu
     @Override
     public void run(SlashCoursesApplicationConfiguration configuration, Environment environment) {
 
+        Resource.loadProperties('resource.properties')
         final DBIFactory factory = new DBIFactory()
         final DBI jdbi = factory.build(environment, configuration.getDatabase(), "jdbi")
 
